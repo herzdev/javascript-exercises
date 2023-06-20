f63db463e4835f6afb277cf080939ac65ae88d5a
Peter Herzenberger <131166301+herzdev@users.noreply.github.com>
2023-06-20 11:15:39 +0200

    Finish 02_repeatString tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,22 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+/*
+PSEUDOCODE:
+Erstelle eine Hilfsvariable namens finalString = "";
+Erstelle eine for-loop die num-Mal durchläuft
+Mit jedem Durchlauf füge der Hilfsvariable finalString den übergebenen string hinzu: finalstring += string
+returne nach der for-loop finalString
+*/
+    if(num < 0) {
+        return "ERROR";
+    } else {
+        let finalString = "";
+        
+        for(let i = 1; i <= num; i++) {
+            finalString += string;
+        }
+        return finalString; 
+    }
+    
 };
 
 // Do not edit below this line
