373b69e28e51ba3426c876d8f4f1ba7a1cfa7b4a
Peter Herzenberger <131166301+herzdev@users.noreply.github.com>
2023-06-20 21:20:59 +0200

    Finish 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  
+  // let celsius = ((fahrenheit - 32) * 5 / 9);
+
+  // if(celsius == 0) {
+  //   return celsius.toFixed(0);
+  // } else {
+  //   return celsius.toFixed(1);
+  // }
+  
+  return Number(((fahrenheit - 32) * 5 / 9).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  return Number((celsius * 9/5 + 32).toFixed(1)); 
 };
 
 // Do not edit below this line
