932df11665fd1f8280f8f9a007624ac958a75660
Peter Herzenberger <131166301+herzdev@users.noreply.github.com>
2023-06-20 21:01:58 +0200

    Finish 05_sumAll TDD

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(startingNumber, endingNumber) {
 
+    let sum = 0;
+
+    if(startingNumber < 0 || endingNumber < 0) {
+        return "ERROR";
+    }
+
+    // console.log(typeof(startingNumber));
+    // console.log(typeof(endingNumber));
+
+
+    if(typeof(startingNumber) != "number" || typeof(endingNumber) != "number") {
+        return "ERROR";
+    }
+
+    if(startingNumber > endingNumber) {  
+        for(let i = startingNumber; i >= endingNumber; i--) {
+            sum += i;      
+        }
+    } else {
+        for(let i = startingNumber; i <= endingNumber; i++) {
+            sum += i;      
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
