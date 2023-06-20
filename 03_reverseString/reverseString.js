b183e4b353e5ad21f4dfb24511708437c2b7b0d8
Peter Herzenberger <131166301+herzdev@users.noreply.github.com>
2023-06-20 11:26:46 +0200

    Finish 03_reverString TDD cases

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,17 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    
+/*
+PSEUDOCODE
+Erstelle leere Variable namens manipulatedString;
+.split() den string und weise ihn manipulatedString zu
+.reverse() manipulatedString
+.join() manipulatedString, damit aus Array wieder ein String wird
+*/
+let manipulatedString;
+
+manipulatedString = string.split("").reverse().join("");
+
+return manipulatedString;
 
 };
 
