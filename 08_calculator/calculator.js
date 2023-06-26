d77b685c8f92739ba07309e1da65292766ef048d
Peter Herzenberger <131166301+herzdev@users.noreply.github.com>
2023-06-26 19:43:57 +0200

    Finish 08_calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	if (arr.length <= 0) {
+    return 0;
+  }
+  return arr.reduce((a, b) => a + b);
 };
 
-const multiply = function() {
-
+const multiply = function(a, b) {
+  // return arguments.length;
+  let sum = 1;
+  for(let i = 0; i <= (arguments.length - 1); i++) {
+    sum *= arguments[i];
+  }
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num == 0) {
+    return 1;
+  } 
+  /*
+  ALGO
+  erstelle ein leeres array
+  loope von num - 1
+  füge nach jeder iteration die aktuelle zahl in einem array hinzu
+  reduce das array auf das faktorielle
+  */
+
+  let arr = [];
+  for(let i = num; i >= 1; i--) {
+    arr.push(i);
+  }
+
+  return arr.reduce((a, b) => a * b);
 };
 
 // Do not edit below this line
