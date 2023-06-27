753f83a04e2b97cf3c3babb175d10cdd6272ccd4
Peter Herzenberger <131166301+herzdev@users.noreply.github.com>
2023-06-27 14:32:04 +0200

    Finish 11_getTheTitles exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,4 +1,15 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
+
+    // Option #1 (with forEach)
+    // const outputArray = [];
+
+    // arr.forEach((element, index) => {
+    //     outputArray.push(element.title);
+    // })
+    // return outputArray;
+
+    // Option #2 (with .map())
+    return arr.map(element => element.title);
 
 };
 
