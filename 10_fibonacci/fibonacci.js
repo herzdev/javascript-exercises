a89f8435db527b378b3f945248b7be0e4448ad7f
Peter Herzenberger <131166301+herzdev@users.noreply.github.com>
2023-06-27 14:15:53 +0200

    Finish 10_fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(size) {
 
+// 1, 1, 2, 3, 5, 8, 13, 21, ...
+//args: gewünschtes Element = Länge der Fibonacci Reihenfolge
+/*
+
+Array Zusammenstellung final:
+Index 0 = 1
+Index 1 = 1
+Index 2 = Index 0 + Index 1
+Index 3 = Index 1 + Index 2
+Index 4 = Index 2 + Index 3
+...
+*/
+
+    const fibonacciNumbers = [1, 1];
+    
+    if(size > 0) {
+        for(let i = 2; i < Number(size); i++) { 
+            fibonacciNumbers[i] = (fibonacciNumbers[i-1] + fibonacciNumbers[i-2]);
+        }
+        return fibonacciNumbers[size-1];
+    }
+    return "OOPS";     
+    
 };
 
 // Do not edit below this line
